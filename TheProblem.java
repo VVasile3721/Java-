bfbd5a13403af2a103241de5e27c82c581b339c0
VVasile3721 <31519215+VVasile3721@users.noreply.github.com>
2017-09-03 18:26:28 +0300

    Commit java problem

---
+++ TheProblem.java
@@ -0,0 +1,22 @@
+public class TheProblem {
+
+	public static void main(String[] args) {
+		int[] A = new int[5];
+		A[0]=4;
+		A[1]=5;
+		A[2]=10;
+		A[3]=11;
+		A[4]=6;
+		int M = -1;
+		for(int i=0; i<A.length; i++) {
+			for(int j=i+1; j<A.length; j++) {
+				if( A[i]-A[j] == M) {
+					System.out.print(i+"-"+j+" "); 
+				}
+			}
+		}
+
+	}
+
+}
+
